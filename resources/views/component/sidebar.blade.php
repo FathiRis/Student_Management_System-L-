e49b10624d3559b1f319efc50ed45f0ea10f00de
FathiRis <risnask45@gmail.com>
2026-02-10 14:58:18 +0530

    Initial Commit

---
+++ resources/views/component/sidebar.blade.php
@@ -0,0 +1,108 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+        <!--begin::Sidebar Brand-->
+        <div class="sidebar-brand">
+          <!--begin::Brand Link-->
+          <a href="" class="brand-link">
+            <!--begin::Brand Image-->
+            <img
+              src="{{ asset('assets/img/AdminLTELogo.png')}}"
+              alt="AdminLTE Logo"
+              class="brand-image opacity-75 shadow"
+            />
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">@stack('side_head')</span>
+            <!--end::Brand Text-->
+          </a>
+          <!--end::Brand Link-->
+        </div>
+        <!--end::Sidebar Brand-->
+        <!--begin::Sidebar Wrapper-->
+        <div class="sidebar-wrapper">
+          <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+              class="nav sidebar-menu flex-column"
+              data-lte-toggle="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+              <li class="nav-item menu-open">
+                <a href="#" class="nav-link active">
+                  <p>
+                    Student
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="{{ route('student.register') }}" class="nav-link active">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Student Register</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./index2.html" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Student Addmission</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./index3.html" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Student List</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li class="nav-item menu-open">
+                <a href="#" class="nav-link active">
+                  <p>
+                    Teacher
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="./index.html" class="nav-link active">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Teacher Register</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./index3.html" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Teacher List</p>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li class="nav-item menu-open">
+                <a href="#" class="nav-link active">
+                  <p>
+                    Exam
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="./index.html" class="nav-link active">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Exam Addmission</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./index2.html" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>Addmission  List</p>
+                    </a>
+                  </li>
+                </ul>
+            </ul>
+            <!--end::Sidebar Menu-->
+          </nav>
+        </div>
+        <!--end::Sidebar Wrapper-->
+      </aside>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+@push('page_title')
+Dashboard
+@endpush
+@push('page_head')
+Dashboard
+@endpush
+@push('side_head')
+Admin
+@endpush
+@section('content')
+
+
+@endsection
+
+            
+          
